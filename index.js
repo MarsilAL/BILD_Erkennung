5c79b86762e648610e484bd7217f2339a52d4536
Tim Looff <giganticset@protonmail.com>
2019-10-09 16:24:52 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const PORT = 8080; //Constante PORT auf 8080 festlegen
+const parser = require('body-parser');
+const express = require('express');
+const classifier = require('./classifier');
+
+
+//konfigurieren
+const webserver = express();
+webserver.use(parser.urlencoded({ extended: false})); //Weisst Express an, text zu verarbeiten
+
+webserver.post('/api/classify',classifier.handle ); //Den Webserver anweisen, etwas anzunehmen (post)
+
+webserver.use(express.static('public')); //Express anweisen, das Verzeichniss public zu benutzen
+
+//starten
+webserver.listen(PORT, function() {
+    console.log('hello from listen')
+
+  
+})
