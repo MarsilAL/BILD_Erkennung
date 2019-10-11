5103e80c9c87057af421c154da487240deda1957
Alashkar <alashkar.marsil@gmail.com>
2019-10-11 13:30:24 +0200

    Heroku

--- classifier.js
+++ classifier.js
@@ -8,22 +8,20 @@ classifier.learn('Torwart Florian beschützte Nadine vor ihrem Ex', 'BILD')
 classifier.learn('Schuh-Ärger für Neuer', 'BILD')
 // WELT
 classifier.learn('Täter versuchten, in Synagoge einzudringen – dann schossen sie auf Passanten', 'WELT')
-classifier.learn('Türkei startet Offensive in Nordsyrien', 'WELT')
+classifier.learn(' startet Offensive in Nordsyrien', 'WELT')
 classifier.learn('Kramp-Karrenbauers gefährlichste Reise', 'WELT')
 classifier.learn('Thomas Müller nährt Spekulationen über einen Wechsel', 'WELT')
 classifier.learn('Thomas Müller will den FC Bayern angeblich schnell verlassen', 'WELT')
 
-var A=1;
-var B=2;
 
 function handle(request, response) {
     // 1 get text
     var text = request.body.input;
-    
+
     // use the classifier to categorize the text
     var category = classifier.categorize(text)
-    
-    
+
+
     // return the category
 
     response.send(category);
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8080; //Constante PORT auf 8080 festlegen
+const PORT = process.env.PORT || 8080; //Constante PORT auf 8080 festlegen
 const parser = require('body-parser');
 const express = require('express');
 const classifier = require('./classifier');
@@ -6,15 +6,15 @@ const classifier = require('./classifier');
 
 //konfigurieren
 const webserver = express();
-webserver.use(parser.urlencoded({ extended: false})); //Weisst Express an, text zu verarbeiten
+webserver.use(parser.urlencoded({ extended: false })); //Weisst Express an, text zu verarbeiten
 
-webserver.post('/api/classify',classifier.handle ); //Den Webserver anweisen, etwas anzunehmen (post)
+webserver.post('/api/classify', classifier.handle); //Den Webserver anweisen, etwas anzunehmen (post)
 
 webserver.use(express.static('public')); //Express anweisen, das Verzeichniss public zu benutzen
 
 //starten
-webserver.listen(PORT, function() {
+webserver.listen(PORT, function () {
     console.log('hello from listen')
 
-  
+
 })
